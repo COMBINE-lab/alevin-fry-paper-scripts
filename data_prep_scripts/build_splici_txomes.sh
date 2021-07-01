76f3892044e7a4adbdadf02352e376649408d473
mohsenzakeri <mohsen.zakery@gmail.com>
2021-07-01 11:01:33 -0400

    Fix splici scripts

--- data_prep_scripts/build_splici_txomes.sh
+++ data_prep_scripts/build_splici_txomes.sh
@@ -17,7 +17,6 @@ fi
 top_dir=$(jq -r '.top_dir' ${config})
 refs_dir=$top_dir/refs
 flank_trim_length=$(jq -r '.flank_trim_length' ${config})
-
 while IFS=, read -r name ref_dir ind_type read_length extra_spliced; do
 	gtf_path="$refs_dir/$ref_dir/genes/genes.gtf"
 	genome_path="$refs_dir/$ref_dir/fasta/genome.fa"
@@ -25,10 +24,10 @@ while IFS=, read -r name ref_dir ind_type read_length extra_spliced; do
 
 	let flank_length=$read_length-$flank_trim_length
 	fasta="$refs_dir/$ref_dir/transcriptome_splici/transcriptome_splici_fl$flank_length.fa"
-    	extra_spliced_seqs = ../mito_seqs/$extra_spliced
+	extra_spliced_seqs=../mito_seqs/$extra_spliced
 	if [ ! -f $fasta ]; then
-	    cmd="Rscript build_splici_txome.R $gtf_path $genome_path $read_length $flank_trim_length $splici_dir $extra_spliced_seqs"
-    	echo $cmd
-    	eval $cmd
+		cmd="Rscript build_splici_txome.R $gtf_path $genome_path $read_length $flank_trim_length $splici_dir $extra_spliced_seqs"
+		echo $cmd
+		eval $cmd
 	fi
 done < $refs_csv
