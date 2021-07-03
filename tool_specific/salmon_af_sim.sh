e67a45ffc26a246f2c62cf9df2da81afd8e74b79
mohsenzakeri <mohsen.zakery@gmail.com>
2021-07-03 16:23:38 -0400

    Fix sal specific script

--- tool_specific/salmon_af_sim.sh
+++ tool_specific/salmon_af_sim.sh
@@ -15,12 +15,11 @@ fry=$(jq -r '.alevinfry_binary' ${config})
 
 splici_index=$top_dir/indices/human-cr3/salmon_fl91_index
 txomic_index=$top_dir/indices/human-cr3/salmon_txome_index
-tmp_dir=$top_dir/_tmp
 results_dir=$top_dir/results
 
-output_dir=$tmp_dir
-fastq_dir=$top_dir/samples/pbmc_5k_sims_human_CR_3.0.0_MultiGeneNo_rl91
-mkdir -p $out_dir
+output_dir=$top_dir/results
+fastq_dir=$top_dir/samples/pbmc_5k_sims_human_CR_3.0.0_MultiGeneNo_rl91/
+mkdir -p $output_dir
 
 t2g=$top_dir/indices/human-cr3/kb_index/t2g.txt
 t2g_3cols=$top_dir/indices/human-cr3/salmon_fl91_index/t2g_3col.tsv
@@ -36,12 +35,12 @@ mkdir -p $output
 mkdir -p $output/logs
 
 cmd="/usr/bin/time -v -o $output/logs/pseudoalignment.time $salmon alevin -l ISR \
-        -i $splici_index -1 $reads1 -2 $reads2 -o $output -p 16 --chromiumV3  --rad"
+        -i $splici_index -1 $read1 -2 $read2 -o $output -p 16 --chromiumV3  --rad"
 echo $cmd
 eval $cmd
 
-./run_alevinfry.sh $fry $output $t2g_3cols $threads knee
-./run_alevinfry.sh $fry $output $t2g_3cols $threads unfilt $unf_list
+bash ./run_alevinfry.sh $fry $output $t2g_3cols $threads knee
+bash ./run_alevinfry.sh $fry $output $t2g_3cols $threads unfilt $unf_list
 
 mv $output/quant_unfilt $results_dir/alevin_fry/sim_data/fry_sla_unfilt_quant_usa_cr-like
 mv $output/quant_knee $results_dir/alevin_fry/sim_data/fry_sla_knee_quant_usa_cr-like
@@ -55,10 +54,8 @@ cmd="/usr/bin/time -v -o $output/logs/pseudoalignment.time $salmon alevin -l ISR
 echo $cmd
 eval $cmd
 
-./run_alevinfry.sh $fry $output $t2g $threads knee
-./run_alevinfry.sh $fry $output $t2g $threads unfilt $unf_list
+bash ./run_alevinfry.sh $fry $output $t2g $threads knee
+bash ./run_alevinfry.sh $fry $output $t2g $threads unfilt $unf_list
 
 mv $output/quant_unfilt $results_dir/alevin_fry/sim_data/fry_unfilt_quant_txome_cr-like
 mv $output/quant_knee $results_dir/alevin_fry/sim_data/fry_knee_quant_txome_cr-like
-
-rm -r $tmp_dir
