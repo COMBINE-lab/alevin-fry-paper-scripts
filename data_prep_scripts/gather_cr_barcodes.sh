ab5901b2d6181f520c7884029ff4b6b906318853
mohsenzakeri <mohsen.zakery@gmail.com>
2021-07-05 00:07:51 -0400

    Organize cr barcode script

--- data_prep_scripts/gather_cr_barcodes.sh
+++ data_prep_scripts/gather_cr_barcodes.sh
@@ -8,13 +8,18 @@ else
 fi
 
 top_dir=$(jq -r '.top_dir' ${config})
-mkdir -p $top_dir/permit_lists
+permit_dir="$top_dir/permit_lists"
+mkdir -p $permit_dir
+
+tmp_dir="$top_dir/_tmp"
+mkdir $tmp_dir
 
 ##cellranger v2 and v3 barcodes
-wget https://raw.githubusercontent.com/10XGenomics/cellranger/master/lib/python/cellranger/barcodes/translation/3M-february-2018.txt.gz
-gunzip 3M-february-2018.txt.gz
-cut -f1 3M-february-2018.txt > $top_dir/permit_lists/10xv3barcodes.txt
-rm 3M-february-2018.txt
+curl -L https://raw.githubusercontent.com/10XGenomics/cellranger/master/lib/python/cellranger/barcodes/translation/3M-february-2018.txt.gz -o $tmp_dir/3M-february-2018.txt.gz
+gunzip $tmp_dor/3M-february-2018.txt.gz
+cut -f1 $tmp_dir/3M-february-2018.txt > $permit_dir/10xv3barcodes.txt
 
-wget https://raw.githubusercontent.com/10XGenomics/cellranger/master/lib/python/cellranger/barcodes/737K-august-2016.txt
-mv 737K-august-2016.txt $top_dir/permit_lists/10xv2barcodes.txt
+curl -L https://raw.githubusercontent.com/10XGenomics/cellranger/master/lib/python/cellranger/barcodes/737K-august-2016.txt -o $tmp_dir/737K-august-2016.txt
+mv $tmp_dir/737K-august-2016.txt $permit_dir/10xv2barcodes.txt
+
+rm -r $tmp_dir
