41f41507f410a106d2e49ed3711f0ebf6792b794
mohsenzakeri <mohsen.zakery@gmail.com>
2021-07-05 00:02:34 -0400

    Minor

--- tool_specific/star_gzip.sh
+++ tool_specific/star_gzip.sh
@@ -7,18 +7,31 @@ else
 	exit
 fi
 
-top_dir=$(jq -r '.top_dir' ${top_dir})
+top_dir=$(jq -r '.top_dir' ${config})
 
 samples="dr_pineal_s2 mouse_placenta"
 for sample in $samples; do
-	gzip -k $top_dir/results/star_solo/$sample/star_solo/Solo.out/Gene/raw/barcodes.tsv
-	gzip -k $top_dir/results/star_solo/$sample/star_solo/Solo.out/Gene/raw/features.tsv
-	gzip -k $top_dir/results/star_solo/$sample/star_solo/Solo.out/Gene/raw/matrix.mtx
+	cmd="gzip -k $top_dir/results/star_solo/$sample/star_solo/Solo.out/Gene/raw/barcodes.tsv"
+	echo $cmd
+	eval $cmd
+	cmd="gzip -k $top_dir/results/star_solo/$sample/star_solo/Solo.out/Gene/raw/features.tsv"
+	echo $cmd
+	eval $cmd
+	cmd="gzip -k $top_dir/results/star_solo/$sample/star_solo/Solo.out/Gene/raw/matrix.mtx"
+	echo $cmd
+	eval $cmd
 	if [ $sample == "dr_pineal_s2" ]; then
-		for type in "1mm_dir exact"; do
-			gzip -k $top_dir/results/star_solo/$sample/star_solo_$type/Solo.out/Gene/raw/barcodes.tsv
-			gzip -k $top_dir/results/star_solo/$sample/star_solo_$type/Solo.out/Gene/raw/features.tsv
-			gzip -k $top_dir/results/star_solo/$sample/star_solo_$type/Solo.out/Gene/raw/matrix.mtx
+		modes="1mm_dir exact"
+		for type in $modes; do
+			cmd="gzip -k $top_dir/results/star_solo/$sample/star_solo_$type/Solo.out/Gene/raw/barcodes.tsv"
+			echo $cmd
+			eval $cmd
+			cmd="gzip -k $top_dir/results/star_solo/$sample/star_solo_$type/Solo.out/Gene/raw/features.tsv"
+			echo $cmd
+			eval $cmd
+			cmd="gzip -k $top_dir/results/star_solo/$sample/star_solo_$type/Solo.out/Gene/raw/matrix.mtx"
+			echo $cmd
+			eval $cmd
 		done
 	fi
 done
